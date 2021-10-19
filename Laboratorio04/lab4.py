b856ca42a15d0c656bf34681febd0cd46cb006cc
julio-1610 <jpaucarc@unsa.edu.pe>
2021-10-19 17:47:45 -0500

    Avance del Ejercicio04 y adición de casos de prueba

--- Laboratorio04/lab4.py
+++ Laboratorio04/lab4.py
@@ -1,20 +1,13 @@
-def greatestNumber(array): #O(n²)
-  for i in array:
-    isIValTheGreatest = True
-    for j in array:
-      if j > i:
-        isIValTheGreatest = False
-    if isIValTheGreatest:
-      return i
-    
 # Ejercicio01
-def greatestNumb(array): #O(n)
+def greatestNumber(array): #O(n)
   # Buscará dentro del array el mayor número
   max = array[0]
   for i in array:
     if (i != array[0] and i > max):
       max = i
   return max
+res = greatestNumber([2, 1 , 4, 5, 7])
+print(res)
 
 # Ejercicio02
 def every_other(array):
@@ -22,8 +15,8 @@ def every_other(array):
     if index % 2 == 0:
       for ob in array:
         print(str(num) + " + " + str(ob))
-
   # Complejidad de O(n²)
+every_other([1, 2, 3, 4, 5])
 
 # Ejercicio03
 def twoSum(array):
@@ -33,6 +26,34 @@ def twoSum(array):
         return True
   
   return False
+  # Complejidad de O(n²)
 
-res = twoSum([1, 8, 9, 2, 7, 3])
+res = twoSum([1, 6, 3, 5, 7])
 print(res)
+
+# Ejercicio04
+def find_needle(needle, haystack):
+  nindex = 0
+  hindex = 0
+
+  found_needle = True
+  while hindex < len(haystack):
+    if needle[nindex] == haystack[hindex]:
+      while needle_index < len(needle):
+        if needle[nindex] != haystack[hindex + nindex]:
+          found_needle = False
+          break
+
+      nindex += 1
+    if found_needle: 
+      return True
+
+    hindex += 1
+
+  return False
+  # Complejidad de O(n²)
+
+res = find_needle("fgh", "abcdefghi")
+print(res)
+      
+      
